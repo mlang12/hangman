47e0416f425a64a89a63194776ffa10b55a38db7
Mark A Lang <mlang12@Marks-MBP.home>
2017-04-11 11:19:16 -0400

    made stable and refined layout

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hangman
+
+The classic game is back but this time the stakes are higher...
+
+The world is heating up and expanding quickly... with every wrong guess the world grows in size!
+
+Figure out the word before your guesses run out and save the planet. 
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,8 @@
+//Initialize the word libary and user input variables
+var wordLib = ['excited', 'happy', 'sad', 'confused', 'tired'];
+var userInput = '';
+
+//Initialize the game object
 var hangmanGame = {
 	livesLeft: 10,
 	lifeLimit: 10,
@@ -5,18 +10,27 @@ var hangmanGame = {
 	guessStatus: [],
 	guessed: [],
 	playedWords: [],
-	validChar: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+	validChar: [
+		'A','B','C','D','E',
+		'F','G','H','I','J',
+		'K','L','M','N','O',
+		'P','Q','R','S','T',
+		'U','V','W','X','Y',
+		'Z'],
 	gameStatus: false,
-	message: 'Type a letter to play it.',
+	message: 'Type any letter to get started.',
+	message2: '-',
 	hangmanSize: 50,
+	explodeSize: 0,
 	hangmanSizeDefault: 50,
-	growthRate: 18,
+	explodeSizeDefault: 0,
+	explode: 200,
+	growthRate: 15,
 	worldVisible: 'visible',
 	explodeVisible: 'hidden',
 	scoreboard: {
 		wins:0,
 		losses:0,
-		resets:0
 	},
 
 	// function creates the current guess status display text i.e. 'ex__mple'
@@ -27,7 +41,7 @@ var hangmanGame = {
 				curStat[i] = input;
 			}
 		}
-		return curStat
+		return curStat;
 	},
 
 	//When user resets game it calls this function which resets values and selects new word
@@ -37,16 +51,20 @@ var hangmanGame = {
 		this.currentWord = this.getNewWord(wordLib,this.playedWords);
 		this.guessed = [];
 		this.gameStatus = true;
-		this.message = 'Type a letter to play it.'
-		this.hangmanSize = this.hangmanSizeDefault
+		this.message = 'Type any letter to get started.';
+		this.message2 = '-'
+		this.hangmanSize = this.hangmanSizeDefault;
+		this.explodeSize = this.explodeSizeDefault;
+		this.explodeVisible = 'hidden';
+		this.worldVisible = 'visible';
 		this.clearBoard();
 		this.renderGameState();
-		console.log('Game was reset' + Date())
+		console.log('Game was reset' + Date());
 	},
 
 	// Function will randomly select a word from the word library and return it excluding played words
 	getNewWord: function (wordLibrary, played){
-		var rdmWord = wordLibrary[Math.floor((Math.random() * wordLibrary.length))];
+		var rdmWord = wordLibrary[Math.floor((Math.random() * wordLibrary.length))].toUpperCase();
 		if(played.indexOf(rdmWord) > -1){
 			this.getNewWord(wordLib, this.playedWords);
 		}
@@ -58,73 +76,75 @@ var hangmanGame = {
 
 		// If the game is over or not active then message the game must be reset 
 		if(this.gameStatus === false){
-			this.message = 'Must Reset the Game.'
-			this.renderGameState();
-		} else if(this.validChar.indexOf(input) > -1 && this.guessed.indexOf(input) === -1 && this.gameStatus === true){
-			//If the input is valid (lowercase letter) and hasn't been guessed and te game is active proceed with gameplay
-			this.guessed.push(input);
+			this.message = 'Must Reset the Game.';
+
+		//If the input is valid (lowercase letter) and hasn't been guessed and te game is active proceed with gameplay
+		} else if (this.validChar.indexOf(input) > -1 && this.guessed.indexOf(input) === -1 && this.gameStatus === true){
 
 			//check if the guessed letter is in the current word
 			if (this.currentWord.indexOf(input) > -1){
 				this.guessStatus = this.buildGuessStatus(input);
 				
 				//check if the user won the game on this guess and execute end of game calls
-				if(this.checkWin()){
-					this.scoreboard.wins++
-					this.gameStatus = false
-					this.message = 'You won the game! The world has cooled off and shrunk to normal size.'
+				if (this.checkWin()){
+					this.scoreboard.wins++;
+					this.gameStatus = false;
+					this.hangmanSize = this.hangmanSizeDefault
+					this.message = 'You won the game! The world has cooled off and shrunk to normal size.';
+					this.message2 = 'Push the \'New Game \' button to play again.'
 				} 
 
-				this.renderGameState();
-
 			} else {
-				//Wrong guess - grows size of the world, lowers life
+				//Wrong guess - grows size of the world, lowers life, and stores guess to 'guessed'
+				this.guessed.push(input);
 				this.growHangman();
 				this.livesLeft--;
 
-				//When life is 0 then end the game with a loss
-				if(this.livesLeft === 0){
+				//When life is 0 then end the game with a loss and execute end of game calls
+				if (this.livesLeft === 0){
 					this.scoreboard.losses++;
 					this.gameStatus = false;
 					this.worldVisible = 'hidden';
 					this.hangmanSize = 0;
 					this.explodeVisible = 'visible';
+					this.explodeSize = this.explode;
 					this.message = 'Sorry, you lost and the world exploded!';
+					this.message2 = 'Push the \'New Game \' button to play again.'
 				} 
-
-				this.renderGameState();
 			}
 		}
+		this.renderGameState();
 	},
 	
 	// This function renders game info to the DOM
 	renderGameState: function () {
 		document.getElementById('currentwordstatus').innerHTML = this.guessStatus.join(' ');
-		document.getElementById('pastguesses').innerHTML = this.guessed.join(',');
+		document.getElementById('pastguesses').innerHTML = this.guessed.join(', ');
 		document.getElementById('livesleft').innerHTML = this.livesLeft;
 		document.getElementById('msg').innerHTML = this.message;
+		document.getElementById('msg2').innerHTML = this.message2;
 		document.getElementById('wincount').innerHTML = this.scoreboard.wins;
 		document.getElementById('losscount').innerHTML = this.scoreboard.losses;
-		//document.getElementById('resetcount').innerHTML = this.scoreboard.resets;
 		document.getElementById('world').style.visibility = this.worldVisible;
 		document.getElementById('world').style.fontSize = this.hangmanSize + 'px';
 		document.getElementById('explode').style.visibility = this.explodeVisible;
+		document.getElementById('explode').style.fontSize = this.explodeSize + 'px';
 	},
 
 	// This function will put blank space for each letter in the current word being guessed
 	clearBoard: function() {
-		this.guessStatus = []
+		this.guessStatus = [];
 		for(var i = 0; i < this.currentWord.length; i++){
-			this.guessStatus.push(" ___ ");
+			this.guessStatus.push(" _ ");
 		};
 	},
 
 	//checks if the state of the game is won or not
 	checkWin: function() {
 		if(this.currentWord === this.guessStatus.join('')){
-			return true
+			return true;
 		}
-		return false
+		return false;
 	},
 
 	//Increases the globe size by defined growth rate
@@ -133,23 +153,23 @@ var hangmanGame = {
 	}
 }
 
-//Initialize the word libary and user input variables
-var wordLib = ['excited', 'happy', 'sad', 'confused', 'tired'];
-var userInput = '';
+//Loads game on page load
+document.onload = hangmanGame.reset()
 
 //Listens for users input and passes to the game object for handling
 document.addEventListener('keydown', function(){
-	userInput = event.key.toLowerCase();
+	userInput = event.key.toUpperCase();
 	hangmanGame.play(userInput);
 });
 
 //Listens for the user to reset the game
 document.getElementById("reset").addEventListener("click", function(){
-	hangmanGame.reset()
+	hangmanGame.reset();
 });
 
 // Prints various values to console for debug
-/*function gameLog() {
+/*
+function gameLog() {
 	console.log('*************************************')
 	console.log(Date())
 	console.log("Lives Left: " + hangmanGame.livesLeft);
@@ -160,8 +180,6 @@ document.getElementById("reset").addEventListener("click", function(){
 	console.log("Scoreboard: "+ hangmanGame.scoreboard.wins + " " + hangmanGame.scoreboard.losses);
 	console.log("Earth Size: " + hangmanGame.hangmanSize);
 	console.log("Guessed Words: " + hangmanGame.playedWords.join(','))
-	
-
 }*/
 
 
